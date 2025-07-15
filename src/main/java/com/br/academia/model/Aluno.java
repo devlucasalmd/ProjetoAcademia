03cdf10074f7606aea5472a54c6293239f58227e
devlucasalmd <lucas.amaral.almd@gmail.com>
2025-07-15 18:38:52 -0300

    feat(Aluno): Criando Entidade Aluno e seus atributos

--- src/main/java/com/br/academia/model/Aluno.java
+++ src/main/java/com/br/academia/model/Aluno.java
@@ -1,5 +1,22 @@
 package com.br.academia.model;
 
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Aluno {
 
+	private Long id;
+	private String nome;
+	private LocalDate dataNasc;
+	private String cpf;
+	private Genero genero	;
+	private String Telefone;
+	private String email;
+	private Boolean ativo;
+	private Endereco endereco;
+	private Matricula matricula;
 }
