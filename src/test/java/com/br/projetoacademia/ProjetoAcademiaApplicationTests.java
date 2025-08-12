dd1a7d2e3e14b20379fbc3da55d3e2acf9032bd5
devlucasalmd <lucas.amaral.almd@gmail.com>
2025-08-11 21:07:13 -0300

    first commit

---
+++ src/test/java/com/br/projetoacademia/ProjetoAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projetoacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
